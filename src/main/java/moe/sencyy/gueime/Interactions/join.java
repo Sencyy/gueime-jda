9a737f81bc1647db9b492200b6bf62d3875b7fce
Sencyy <l1.nx@outlook.com>
2023-01-06 19:56:45 -0300

    gueime 4.1.0

---
+++ src/main/java/moe/sencyy/gueime/Interactions/join.java
@@ -0,0 +1,12 @@
+package moe.sencyy.gueime.Interactions;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GenericGuildMemberEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class join extends ListenerAdapter {
+
+
+    public void
+}
